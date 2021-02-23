e560d8ef016b06de5a654732f32f9c2f95f2f222
Bonchay Ouk <bonchayouk@gmail.com>
2021-02-23 15:20:55 +0700

    card item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kabob_ui/hexcolor.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-    
         primarySwatch: Colors.blue,
-   
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,44 +30,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
+  Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
   }
+
+  Color _color = HexColor("#03A9F4");
+  Color _secondColor = HexColor("#00A4E5");
+
+  Color _third = HexColor("#F5F5F5");
+//  List<Color> facebookMessenger = [
+//     _color,
+//     Color(0xFF0072ff)
+//   ];
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       appBar: AppBar(
-      
         title: Text(widget.title),
       ),
       body: Center(
-       
-        child: Column(
-    
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            height: 180,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [_third, _third]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // Shadow position
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('SELENDRA', style: TextStyle(fontSize: 22)),
+                        Image.asset(
+                          'assets/sel_logo.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                        // Container(
+                        //   padding: const EdgeInsets.all(5.0),
+                        //   decoration: BoxDecoration(
+                        //       shape: BoxShape.circle, color: Colors.white),
+                        //   child:
+                        // ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text('0.009 SEL',
+                          style: TextStyle(
+                            fontSize: 18,
+                          )),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: Text(
+                          '5GEEocqakdLcK9k6eMVyjExvioy1uMmha3Lr2k1jBoZR82nb',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 14.0)),
+                    ),
+                    Icon(
+                      Icons.copy,
+                    )
+                  ],
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
